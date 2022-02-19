35de6fb8ca01421294ef8b039f10c7c3ea5114c4
hashbandicoot4 <hashimh@hotmail.co.uk>
2022-02-19 17:03:58 +0000

    DURRRRRRRRRRRDLE
    DURRRRRRRRRRRDLE

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const { response } = require("app.js");
 
 const app = express();
 // Interact with file system
@@ -9,12 +8,15 @@ app.use(express.static("Client"));
 app.use(express.json());
 
 // Load text file
-const durdle = fs.readFileSync("durdle-allowed-guesses.txt", 
-    {encoding: "utf-8", flag: "r"});
+const durdle = fs.readFileSync("durdle-allowed-guesses.txt", function(err,data) {
+    if(err) throw err;
+    obj = JSON.parse(data);
+});
 
 // Constant random seed per day
 
 const wordSet = ["jeans", "pipes", "ultra", "disco", "unity"];
+wordSet = JSON.parse(wordSet);
 
 // Get all valid words
 app.get("/validWords", function(req, res) {
