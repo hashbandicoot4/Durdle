7ec50529ce5d25642532cb070be7b1228261fa67
hashbandicoot4 <hashimh@hotmail.co.uk>
2022-02-19 16:13:29 +0000

    Durdle Durdle Durdle
    Durdle Durdle Durdle

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-const express = require('express');
+const express = require("express");
+const { response } = require('../../Durham/First_Year/COMP 1111 - Programming/Website Summative Assessment/app');
 
 const app = express();
+// Interact with file system
+const fs = require('fs')
 
 app.use(express.static('Client'));
 app.use(express.json());
 
-app.listen(8080);
+// Load text file
+const durdle = fs.readFileSync("durdle-allowed-guesses.txt", 
+    {encoding: "utf-8", flag: "r"});
 
+console.log(durdle);
+
+// Get 5 words and 
+app.get("/words", function(req, res) {
+    // Depending on the guess we are currently on
+    res.json(wordSet)
+});
+
+app.listen(8080);
