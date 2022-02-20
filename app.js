6f5759a1dcbde42989a12d718c905a45f37da7c1
InfinityWarlock <theo.hintonhallows123@gmail.com>
2022-02-20 01:09:48 +0000

    frontend communicates with backend

--- app.js
+++ app.js
@@ -1,33 +1,39 @@
-const express = require("express");
+import express from 'express';
 
 const app = express();
 // Interact with file system
-const fs = require('fs')
+import fs from 'fs';
 
-app.use(express.static("Client"));
+app.use(express.static('Client'));
 app.use(express.json());
 
 // Load text file
-const durdle = fs.readFileSync("durdle-allowed-guesses.txt", function(err,data) {
-    if(err) throw err;
-    obj = JSON.parse(data);
-    console.log(obj)
-});
+const ValidWords = JSON.parse(fs.readFileSync('./valid-words.json'));
 
-console.log(durdle);
+// const durdle = fs.readFileSync('valid-words.json', function (err, data) {
+//     if (err) throw err;
+//     obj = JSON.parse(data);
+//     console.log(obj);
+//     return obj;
+// });
+
+// console.log(durdle);
+console.log(ValidWords);
 
 // Constant random seed per day
-var wordSet = ["jeans", "pipes", "ultra", "disco", "unity"];
+var wordSet = ['jeans', 'pipes', 'ultra', 'disco', 'unity'];
 var dailyWordSet = JSON.stringify(wordSet);
 
 // Get all valid words
-app.get("/validWords", function(req, res) {
-    res.json(durdle)
+app.get('/validWords', function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.json(ValidWords);
 });
 
 // Get 5 guess words set
-app.get("/dailyWordSet", function(req, res) {
-    res.json(wordSet)
+app.get('/dailyWordSet', function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.json(wordSet);
 });
 
 app.listen(8080);
